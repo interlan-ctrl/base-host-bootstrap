547c57526121beaea3264f521899023d8608f0d7
Chernousov Anton <anton.public.82@gmail.com>
2021-03-07 11:51:31 +0700

    Add sudo to base software

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y upgrade
 
 # Install same base software
-apt-get install -y mc aptitude curl
+apt-get install -y mc aptitude curl sudo
 
 # Install Python for working with Ansible
 apt-get install -y python3 python3-pip
